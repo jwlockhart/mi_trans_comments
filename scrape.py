783ddf0321abf166602978856ff0b62d2afbb381
Jeff Lockhart <jwlockhart@users.noreply.github.com>
2016-05-09 21:24:18 -0400

    documentation

--- scrape.py
+++ scrape.py
@@ -1,3 +1,6 @@
+# Script for scraping the comments data off of the public comment site
+# v0.2
+
 from bs4 import BeautifulSoup as bs
 import urllib2
 import time
@@ -5,9 +8,13 @@ import time
 #number of pages of comments
 pages = 214
 
+#the base url for all pages of comments
 url = 'http://everyvoicecountsmi.org/136/public-comment-on-the-state-board-of-education-draft-statement-and-guidance-on-safe-and-supportive-learning-environments-for-lesbian-gay-bisexual-transgender-and-questioning-lgbtq-students/comment-page-'
+
+#a list for holding pages of comments
 data = []
 
+print 'Downloading comment pages...'
 for i in range(1, pages+1):
     #figure out the address to read next
     addr = url + str(i)
@@ -19,12 +26,12 @@ for i in range(1, pages+1):
     #chill for a bit so as not to overwhelm server
     time.sleep(1)
     
-    #download the page and save it
+    #download the page
     req = urllib2.Request(addr, headers={'User-Agent': 'Magic-Browser'})
     con = urllib2.urlopen(req)
     data.append(con.read())
     
-print 'Done downloading!'
+print 'Completed 100%'
 
 #merge all the webpages into one single giant string
 together = '\n'.join(data)
@@ -33,4 +40,6 @@ print 'Saving the raw html...'
 with open('raw.html', 'w') as fi:
     fi.write(together)
     
-print 'Done saving!
+print 'Done saving!'
+
+#TODO: parse the html
